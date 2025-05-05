660bb1dd698f76780f7f9caa5423ef8c32b62fa3
doyeonkim1997 <0104801@naver.com>
2025-05-06 02:19:41 +0900

    add test.js for optional chaining test

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-console.log("Hello Git!");
-console.log("Hello Git!");
-console.log("Hello Git!");
-console.log("Hello Git!");
-console.log("Hello Git!");
-console
+const data = null;
+console.log(data?.["name"]);
+
+const fruits = ["apple", "banana"];
+console.log(fruits?.[1]);
